876f683a798fdea00ad5ca0f8673b82006574894
allikitten <98057200+allikitten@users.noreply.github.com>
2022-04-06 18:43:59 -0700

    More levels

--- Terrible Tweeters/Assets/Scripts/Monster.cs
+++ Terrible Tweeters/Assets/Scripts/Monster.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
+
 public class Monster : MonoBehaviour
 {
+    [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+
+    bool _hasDied;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(ShouldDieFromCollision(collision))
@@ -14,8 +21,14 @@ public class Monster : MonoBehaviour
 
     bool ShouldDieFromCollision(Collision2D collision)
     {
+        if (_hasDied)
+            return false;
+
         Bird bird = collision.gameObject.GetComponent<Bird>();
         if (bird != null)
+            return true;
+
+        if (collision.contacts[0].normal.y < -0.5)
             return true;
 
         return false;
@@ -23,6 +36,9 @@ public class Monster : MonoBehaviour
 
     void Die()
     {
-        gameObject.SetActive(false);
+        _hasDied = true;
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+        _particleSystem.Play();
+        //gameObject.SetActive(false);
     }
 }
